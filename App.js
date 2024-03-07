4539b3c3b449e7db159b650e88a4e5cbd46b7e8f
Meet Raut <122288042+MeetRaut@users.noreply.github.com>
2024-03-08 00:06:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './Login'
+import LoginComponent from './Login';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginComponent/>
+    </div>
+  );
+}
+
+export default App;
